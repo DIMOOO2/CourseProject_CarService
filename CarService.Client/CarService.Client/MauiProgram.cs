4880d75cc860ffff6228f3e1498a75f95d05d5da
DIMOOO2 <137613944+DIMOOO2@users.noreply.github.com>
2025-03-27 12:31:29 +0300

    Начало верстки клиентского приложения, доработка начальной страницы, экспортирование картинки и изменение названия.

--- CarService.Client/CarService.Client/MauiProgram.cs
+++ CarService.Client/CarService.Client/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using UraniumUI;
 
 namespace CarService.Client
 {
@@ -9,6 +10,8 @@ namespace CarService.Client
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseUraniumUI()
+                .UseUraniumUIMaterial()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
