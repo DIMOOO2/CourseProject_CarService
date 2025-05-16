5704bffca429fa4967c19a20be88fd54b9c9afa4
ISP-22 StudAn <ISP-22@PC-232-10>
2025-05-16 11:48:39 +0300

    Начало работы на приложением администратора

---
+++ CarService.Administrator/Others/Data/WebData.cs
@@ -0,0 +1,64 @@
+﻿using CarService.ApplicationService.Contracts.Responses;
+using CarService.Core.Models;
+using System.Collections.ObjectModel;
+
+
+namespace CarService.Administrator.Others.Data
+{
+    public static class WebData
+    {
+        public static ObservableCollection<AutoPart>? AutoParts { get; set; }
+        public static ObservableCollection<Order>? Orders { get; set; }
+        public static ObservableCollection<Manufacturer>? Manufacturers { get; set; }
+        public static ObservableCollection<Core.Models.Client>? Clients { get; set; }
+
+
+        public static void GetAutoPartsCollection(List<AutoPartResponse>? autoParts)
+        {
+            ObservableCollection<AutoPart> currentAutoParts = new ObservableCollection<AutoPart>();
+            foreach(var item in autoParts!)
+            {
+                currentAutoParts.Add(AutoPart.Create(item.autoPartId, item.autoPartName, item.partNumber,
+                    item.price, item.stockAmount, item.manufacturerId, item.warehouseId).AutoPart);
+            }
+
+            AutoParts = currentAutoParts;
+        }
+
+        public static void GetOrdersCollection(List<OrderResponse>? orders)
+        {
+            ObservableCollection<Order> currentOrders = new ObservableCollection<Order>();
+            foreach (var item in orders!)
+            {
+                currentOrders.Add(Order.Create(item.orderId, item.orderDate, item.orderStatus, 
+                    item.clientId, item.warehouseContratorId).Order);
+            }
+
+            Orders = currentOrders;
+        }
+
+        public static void GetClientCollection(List<ClientResponse> clients)
+        {
+            ObservableCollection<Core.Models.Client> currentClients = new ObservableCollection<Core.Models.Client>();
+            foreach (var client in clients!)
+            {
+                currentClients.Add(Core.Models.Client.Create(client.clientId, client.firstName, client.lastName, client.middleName,
+                    client.phoneNumber, client.email, client.address, client.city, client.organizationId).Client);
+            }
+
+            Clients = currentClients;
+        }
+
+        public static void GetCollectionManufacturer(List<ManufacturerResponse> manufacturers)
+        {
+            ObservableCollection<Manufacturer> currentManufacturers = new ObservableCollection<Manufacturer>();
+            foreach (var manufacturer in manufacturers!)
+            {
+                currentManufacturers.Add(Manufacturer.Create(manufacturer.manufacturerId, manufacturer.manufacturerName, manufacturer.contactInfo).Manufacturer);
+            }
+
+            Manufacturers = currentManufacturers;
+        }
+    }
+}
+
---
+++ CarService.Administrator/Others/Models/OrderInfo.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace CarService.Administrator.Others.Models
+{
+    public class OrderInfo
+    {
+        public OrderInfo(Guid orderId, DateTime orderDate, bool orderStatus)
+        {
+            OrderId = orderId;
+            OrderDate = orderDate;
+            OrderStatus = orderStatus;
+        }
+
+        public Guid OrderId { get; }
+        public DateTime OrderDate { get; }
+        public bool OrderStatus { get; }
+        public Core.Models.Client Client
+        {
+            get
+            {
+                return Data.WebData.Clients!.FirstOrDefault(c => c.ClientId == Data.WebData.Orders!.FirstOrDefault(o => o.OrderId == OrderId)!.ClientId)!;
+            }
+        }
+
+        public Guid WarehouseContractorId { get; }
+
+        public Color ColorStatus
+        {
+            get
+            {
+                if (OrderStatus) return Color.FromArgb("#32cd32");
+                else return Color.FromArgb("#ff1f1f");
+            }
+        }
+
+        public string ArticulNumber
+        {
+            get
+            {
+                byte[] data = OrderId.ToByteArray();
+                return Math.Abs(BitConverter.ToInt32(data, 0)).ToString();
+            }
+        }
+
+        public string GetStatus
+        {
+            get
+            {
+                if (OrderStatus) return "Статус: Завершен";
+                else return "Статус: Не завершен";
+            }
+        }
+
+        public string GetDate
+        {
+            get
+            {
+                return OrderDate.ToLongDateString().ToString();
+            }
+        }
+    }
+}
