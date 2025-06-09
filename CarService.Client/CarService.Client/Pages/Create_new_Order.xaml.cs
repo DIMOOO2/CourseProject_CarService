0f862145308bf8209f8ea99d9cef259010f6652a
DIMOOO2 <137613944+DIMOOO2@users.noreply.github.com>
2025-06-09 21:42:57 +0300

    Delete CarService.Client directory

--- CarService.Client/CarService.Client/Pages/Create_new_Order.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CarService.Client.Pages;
-
-public partial class Create_new_Order : ContentPage
-{
-	public Create_new_Order()
-	{
-		InitializeComponent();
-	}
-}
--- CarService.Client/CarService.Client/Pages/OrderPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CarService.Client.Pages;
-
-public partial class OrderPage : ContentPage
-{
-	public OrderPage()
-	{
-		InitializeComponent();
-	}
-}
